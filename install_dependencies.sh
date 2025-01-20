0fe2219a0dcac0d6e89e084df0bae4dd0ee940bd
benahalkar <benahalkar@gmail.com>
2025-01-19 20:13:11 -0500

    adding installer

---
+++ install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip install FlightRadarAPI
+pip install plotly
+pip install dash
+pip install dash_bootstrap_components
+pip install dash_bootstrap_templates
+pip install pandas
+pip install datetime
+pip install requests
+pip install pyspark
+pip install regex
